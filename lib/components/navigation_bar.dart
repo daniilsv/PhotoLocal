6cae685a2b67eb87162ef9aebec9ce73583bfdb7
Daniil Samoilov <daniilsvin@gmail.com>
2020-04-26 09:01:34 +0300

    add smooth navigatopn bar

--- lib/components/navigation_bar.dart
+++ lib/components/navigation_bar.dart
@@ -13,60 +13,63 @@ class NavigationBar extends StatelessWidget {
   final int currentIndex;
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: PLColors.bg,
-      selectedItemColor: PLColors.accent,
-      unselectedItemColor: PLColors.grey,
-      showUnselectedLabels: true,
-      currentIndex: currentIndex,
-      onTap: (index) {
-        Widget screen;
-        switch (index) {
-          case 0:
-            screen = WallScreen();
-            break;
-          case 1:
-            screen = MapScreen();
-            break;
-          case 2:
-            screen = ChatsScreen();
-            break;
-          case 3:
-            screen = ProfileScreen();
-            break;
-        }
-        Navigator.popUntil(context, (route) => route.isFirst);
-        Navigator.pushReplacement(
-          context,
-          PageTransition(
-            child: screen,
-            type: PageTransitionType.fade,
+    return Hero(
+      tag: "nav_bar",
+      child: BottomNavigationBar(
+        backgroundColor: PLColors.bg,
+        selectedItemColor: PLColors.accent,
+        unselectedItemColor: PLColors.grey,
+        showUnselectedLabels: true,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          Widget screen;
+          switch (index) {
+            case 0:
+              screen = WallScreen();
+              break;
+            case 1:
+              screen = MapScreen();
+              break;
+            case 2:
+              screen = ChatsScreen();
+              break;
+            case 3:
+              screen = ProfileScreen();
+              break;
+          }
+          Navigator.popUntil(context, (route) => route.isFirst);
+          Navigator.pushReplacement(
+            context,
+            PageTransition(
+              child: screen,
+              type: PageTransitionType.fade,
+            ),
+          );
+        },
+        iconSize: 40,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: PLColors.bg,
+            icon: Icon(PhotoLocal.wall),
+            title: Text("Лента", style: PLStyle.textMed),
           ),
-        );
-      },
-      iconSize: 40,
-      items: [
-        BottomNavigationBarItem(
-          backgroundColor: PLColors.bg,
-          icon: Icon(PhotoLocal.wall),
-          title: Text("Лента", style: PLStyle.textMed),
-        ),
-        BottomNavigationBarItem(
-          backgroundColor: PLColors.bg,
-          icon: Icon(PhotoLocal.map),
-          title: Text("Карта", style: PLStyle.textMed),
-        ),
-        BottomNavigationBarItem(
-          backgroundColor: PLColors.bg,
-          icon: Icon(PhotoLocal.chat),
-          title: Text("Сообщения", style: PLStyle.textMed),
-        ),
-        BottomNavigationBarItem(
-          backgroundColor: PLColors.bg,
-          icon: Icon(PhotoLocal.profile),
-          title: Text("Профиль", style: PLStyle.textMed),
-        ),
-      ],
+          BottomNavigationBarItem(
+            backgroundColor: PLColors.bg,
+            icon: Icon(PhotoLocal.map),
+            title: Text("Карта", style: PLStyle.textMed),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: PLColors.bg,
+            icon: Icon(PhotoLocal.chat),
+            title: Text("Сообщения", style: PLStyle.textMed),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: PLColors.bg,
+            icon: Icon(PhotoLocal.profile),
+            title: Text("Профиль", style: PLStyle.textMed),
+          ),
+        ],
+      ),
     );
   }
 }
