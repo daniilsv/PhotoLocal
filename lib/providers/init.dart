73a506c0805f2ad479983c3ca56cede34f9a016b
Daniil Samoilov <daniilsvin@gmail.com>
2020-04-26 05:26:22 +0300

    make init provider with api

--- lib/providers/init.dart
+++ lib/providers/init.dart
@@ -4,6 +4,8 @@ import 'package:photolocal/main.dart';
 import 'package:photolocal/api/api.dart';
 import 'package:photolocal/global/database.dart';
 import 'package:flutter/foundation.dart';
+import 'package:photolocal/models/models.dart';
+import 'package:photolocal/models/serializers.dart';
 import 'package:photolocal/screens/auth/index.dart';
 import 'package:photolocal/screens/map/index.dart';
 import 'package:photolocal/screens/profile_choose/index.dart';
@@ -20,13 +22,16 @@ class InitProvider with ChangeNotifier {
   InitProvider._internal();
   InitState state = InitState.splash;
 
+  Session session;
+
   init() async {
     await DataBase().open();
     var store = stringMapStoreFactory.store("blocs");
     Map<String, dynamic> serialized;
     await Future.wait(<Future>[
       store
-          .findFirst(DataBase.db, finder: Finder(filter: Filter.byKey("user")))
+          .findFirst(DataBase.db,
+              finder: Finder(filter: Filter.byKey("session")))
           .then((value) => serialized = value?.value),
       Future.delayed(Duration(seconds: 2)) // delay for nice splash
     ]);
@@ -34,7 +39,8 @@ class InitProvider with ChangeNotifier {
       Api.init();
       setState(InitState.auth);
     } else {
-      Api.init(""); //TODO: pass here some token
+      session = serializers.deserializeWith(Session.serializer, serialized);
+      Api.init(session.token);
       setState(InitState.inited);
     }
   }
@@ -76,5 +82,12 @@ class InitProvider with ChangeNotifier {
     store.delete(DataBase.db);
     Api.init();
     setState(InitState.auth);
+  }
+
+  setSession(Session _session) async {
+    session = _session;
+    var store = stringMapStoreFactory.store("blocs");
+    await store.record("session").put(
+        DataBase.db, serializers.serializeWith(Session.serializer, session));
   }
 }
