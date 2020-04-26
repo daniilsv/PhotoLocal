ea87af611519280220690d3294af56515ef53dfe
gicha <01gicha01@gmail.com>
2020-04-26 09:30:30 +0300

    add photos mess

---
+++ lib/screens/chat/widgets/photos_message.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+import 'package:photolocal/components/image.dart';
+import 'package:photolocal/models/models.dart';
+import 'package:photolocal/theme/theme.dart';
+
+class PhotosMessage extends StatefulWidget {
+  PhotosMessage(
+    this.photographer, {
+    Key key,
+    this.withEdit,
+    this.withoutEdit,
+    this.withDetalEdit,
+  }) : super(key: key);
+  final Photographer photographer;
+  final List<String> withEdit;
+  final List<String> withoutEdit;
+  final List<String> withDetalEdit;
+
+  @override
+  _PhotosMessageState createState() => _PhotosMessageState();
+}
+
+class _PhotosMessageState extends State<PhotosMessage> {
+  bool tapped = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 4).add(EdgeInsets.only(right: 30)),
+      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+      decoration: BoxDecoration(
+        borderRadius: PLBorders.all16,
+        border: Border.all(color: Color(0xFF979797), width: 0.5),
+      ),
+      alignment: Alignment.center,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            "Ура! ${widget.photographer.name} прислала фото со съёмки. Скорее смотреть их:",
+            style: PLStyle.subheader,
+          ),
+          SizedBox(height: 25),
+          if (widget.withEdit?.isNotEmpty != null)
+            Column(
+              children: [
+                Text(
+                  "С обработкой:",
+                  style: PLStyle.text.copyWith(color: Color(0xFF959595)),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: List.generate(
+                      widget.withEdit.length,
+                      (index) => Container(
+                        padding: EdgeInsets.only(right: 10),
+                        height: 40,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4.5),
+                          child: PLImage(
+                            widget.withEdit[index],
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+          SizedBox(height: 25),
+          if (widget.withoutEdit?.isNotEmpty != null)
+            Column(
+              children: [
+                Text(
+                  "Без обработки:",
+                  style: PLStyle.text.copyWith(color: Color(0xFF959595)),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: List.generate(
+                      widget.withoutEdit.length,
+                      (index) => Container(
+                        padding: EdgeInsets.only(right: 10),
+                        height: 40,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4.5),
+                          child: PLImage(
+                            widget.withoutEdit[index],
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+          SizedBox(height: 25),
+          if (widget.withDetalEdit?.isNotEmpty != null)
+            Column(
+              children: [
+                Text(
+                  "В детальной обработке:",
+                  style: PLStyle.text.copyWith(color: Color(0xFF959595)),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: List.generate(
+                      widget.withDetalEdit.length,
+                      (index) => Container(
+                        padding: EdgeInsets.only(right: 10),
+                        height: 40,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4.5),
+                          child: PLImage(
+                            widget.withDetalEdit[index],
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          SizedBox(height: 12),
+          Center(
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                if (!tapped) {
+                  tapped = true;
+                  setState(() {});
+                  Future.delayed(Duration(seconds: 3), () {
+                    tapped = false;
+                    setState(() {});
+                  });
+                }
+              },
+              child: Padding(
+                padding: EdgeInsets.all(4),
+                child: Text(
+                  "Копировать ссылку на .zip",
+                  style: PLStyle.subheader.copyWith(
+                    color: PLColors.accent.withOpacity(
+                      tapped ? 0.4 : 1.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 12),
+        ],
+      ),
+    );
+  }
+}
